1a32c5b87ee08b94f0be76914442aa6ee75b7fe9
Дмитрий Бычков <dimac090501@hotmail.com>
2018-12-02 19:01:10 +0300

    lex

--- upload.bat
+++ upload.bat
@@ -1,10 +1,12 @@
 @echo off
 set /p str=Enter chapter:
-git checkout "master"
-git pull
 git checkout "dev"
+git merge "master"
+git commit -m "%str%"
 git merge "%str%a"
+git commit -m "%str%"
 git merge "%str%d"
+git commit -m "%str%"
 git add --all
 git commit -m "%str%"
 git push
