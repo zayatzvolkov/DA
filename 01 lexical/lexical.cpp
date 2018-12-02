45f5cf39b02a0b37c0c7194fd8fcb17da8adb68b
Дмитрий Бычков <dimac090501@hotmail.com>
2018-12-02 14:40:21 +0300

    marking

---
+++ 01 lexical/lexical.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** argc)
+{
+    // so start
+    return 0;
+}
--- 03 syntax/syntax.cpp
+++ 03 syntax/syntax.cpp
@@ -5,8 +5,6 @@ using namespace std;
 
 int main(int argc, char ** argc)
 {
-    // so start
-    // I think this is so strange
-    // but...
+
     return 0;
 }
