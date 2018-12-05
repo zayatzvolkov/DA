5d8d4a6045cbd686975ce627dfb8b2ed1232dfea
Дмитрий Бычков <dimac090501@hotmail.com>
2018-12-05 22:01:41 +0300

    lexd

--- 01 lexical/lexical.hpp
+++ 01 lexical/lexical.hpp
@@ -11,6 +11,6 @@
 #ifndef LEXICAL
 #define
 
-enum lexical_types = {RESERVEDWORD=1,NAME,NUMBER,OPERATOR,TERMINAL,LITERAL}
+enum lexical_types = {RESERVEDWORD=1, NAME, NUMBER, OPERATOR, TERMINAL, LITERAL}
 
 #endif /* end of include guard */
